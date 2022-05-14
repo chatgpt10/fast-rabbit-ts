25895859641b212f060290cc64051c1bd8defac5
liuerjian <1913698873@qq.com>
2022-05-14 11:00:35 +0800

    feat:配置@别名

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>New Vue3</h1>
 </template>
 <script setup lang="ts">
-import request from "../utils/request"
+import request from "@/utils/request"
 const test = async () => {
   const res = await request.get('/home/index')
   console.log(res)
