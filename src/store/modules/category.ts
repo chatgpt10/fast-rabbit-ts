ad67ac3dc1b2ab2975e7f149762cd44262d2864c
liuerjian <1913698873@qq.com>
2022-05-15 10:44:26 +0800

    feat:实现导航栏一级和二级的数据渲染

--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,7 +1,35 @@
 import { defineStore } from 'pinia'
+import  request  from '@/utils/request'
+import {ApiRes,CategoryItem} from '@/types/data'
+import { topCategory } from '../constants'
+const defaultCategory = topCategory.map((item)=>{
+  return {
+    name:item
+  }
+})
 export default defineStore('category', {
   state: () => ({
-    money: 100,
-    key:191369
+    list:defaultCategory as CategoryItem[] ,
   }),
+  actions :{
+    async getAllCategory(){
+      const res=await request.get<ApiRes<CategoryItem[]>>('/home/category/head')
+      this.list=res.data.result
+      console.log( this.list);
+    }
+  }
 })
+// const useHomeStore=defineStore('category', {
+//   state: () => ({
+//       list:defaultCategory as CategoryItem[] ,
+//       }),
+//       actions :{
+//         async getAllCategory(){
+//           const res=await request.get<ApiRes<CategoryItem[]>>('/home/category/head')
+//           this.list=res.data.result
+//           console.log( this.list);
+//         }
+//       }
+// })
+
+// export { useHomeStore }
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -14,9 +14,10 @@ import AppHeader from './components/app-header.vue'
 import AppFooter from './components/app-footer.vue'
 // pinia 
 import useStore from '@/store';
+// Pinia数据解构保持响应式
 import { storeToRefs } from 'pinia'
-const store = useStore().category;
-console.log(store.money);
+// const store = useStore().category;
+// console.log(store.money);
 </script>
 
 <style scoped lang="less">
