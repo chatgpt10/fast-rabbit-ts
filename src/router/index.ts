23e2dc548ac5b349b9aeba95a3380fbb85d22fd9
liuerjian <1913698873@qq.com>
2022-05-15 11:06:43 +0800

    feat:Add 二级路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,29 @@
 // 路由文件
 import {createRouter, createWebHashHistory } from 'vue-router'
 import Layout from "@/views/layout/index.vue"
+import Home from "@/views/home/index.vue"
 
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
         {
             path:'/',
-            component:Layout
+            component:Layout,
+            children:[
+               {
+                path:'',
+                component:Home
+               },
+               {
+                   path:'/category/:id',
+                   component:() => import('@/views/category/index.vue')
+               }
+               ,
+               {
+                   path:'/category/sub/:id',
+                   component:() => import('@/views/category/sub.vue')
+               }
+            ]
         },  
         {
             path:'/login',
