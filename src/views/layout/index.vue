0fe45f76c37fffc255ad8a1c451a5672e8dabadf
liuerjian <1913698873@qq.com>
2022-05-16 11:06:49 +0800

    feat:通过vueUse实现吸顶导航

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,8 @@
 <template>
  <AppTopnav></AppTopnav>
   <AppHeader></AppHeader>
+  <!-- 吸顶导航 -->
+   <AppHeaderSticky></AppHeaderSticky>
   <main class="app-body">
     <!-- 路由出口 -->
     <RouterView></RouterView>
@@ -12,6 +14,8 @@
 import AppTopnav from './components/app-topnav.vue'
 import AppHeader from './components/app-header.vue'
 import AppFooter from './components/app-footer.vue'
+// 吸顶导航
+import AppHeaderSticky from './components/app-header-sticky.vue';
 // pinia 
 import useStore from '@/store';
 // Pinia数据解构保持响应式
