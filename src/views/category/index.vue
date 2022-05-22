a812e307f7004efbd811a377456d86247c0edece
liuerjian <1913698873@qq.com>
2022-05-22 11:37:15 +0800

    feat:路由watch，导航栏变化

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -6,18 +6,30 @@
         <XtxBreadItem to="/">首页</XtxBreadItem>
         <XtxBreadItem>分类</XtxBreadItem>
       </XtxBread>
+      {{route.params.id}}
     </div>
   </div>
 </template>
 
 <script lang='ts' setup name="TopCategory">
+import { ref,watch } from "vue";
 // 路由
 import { useRoute } from 'vue-router';
 // pinia数据
 import useStore from '@/store';
 const route=useRoute();
 const { category } = useStore();
-category.getTopCategory(route.params.id as string)
+
+// 监听路由变化
+watch (
+    ()=>route.params.id,
+    (value)=>{
+        if(route.fullPath===`/category/${value}`)
+        category.getTopCategory(value as string)
+    },{
+        immediate:true,
+    }
+)
 </script>
 
 <style scoped lang="less">
